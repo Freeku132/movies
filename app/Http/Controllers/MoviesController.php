1f618c5316ed9074793dc25ead5f704b2c34aa51
Freeku132 <freeku13@gmail.com>
2022-08-28 00:52:04 +0200

    MovieTMDB controller

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -14,6 +14,7 @@ class MoviesController extends Controller
      */
     public function index()
     {
+
         $popularMovies = Http::withToken(config('services.tmdb.token'))
             ->get('https://api.themoviedb.org/3/movie/popular')
             ->json()['results'];
